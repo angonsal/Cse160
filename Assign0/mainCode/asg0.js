30c93d9e7f61c5962dd982e48631d2d41a92ee61
Ashley Gonsalves <ashleygonsalves@eduroam-169-233-207-42.ucsc.edu>
2024-04-04 17:26:37 -0700

    minor fix

--- Assign0/mainCode/asg0.js
+++ Assign0/mainCode/asg0.js
@@ -18,8 +18,8 @@ function main() {
 	// ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
 
 	// Black Canvas 
-	var v1 = new Vector3([2.25, 2.25, 0]); 
-	drawVector(v1, "red");
+	// var v1 = new Vector3([2.25, 2.25, 0]); 
+	// drawVector(v1, "red");
 	
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
