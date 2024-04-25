226f58c700ed32edb7760e61cbc25481134a872c
Ashley Gonsalves <ashleygonsalves@Ashley-Noelle.local>
2024-04-25 03:17:00 -0700

    adding again

---
+++ Assign2/src/TrianglePrism.js
@@ -0,0 +1,22 @@
+class TriangularPrism {
+    constructor() {
+        this.type = 'triangularPrism';
+        this.color = [1.0, 1.0, 1.0, 1.0]; // RGBA color
+        this.matrix = new Matrix4(); // Transformation matrix
+    }
+
+    render() {
+        var rgba = this.color;
+
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+        drawTriangle3D( [0.0,0.0,0.0,   1.0,0.0,0.0,   0.5,0.5,0.0]);
+        drawTriangle3D( [0.0,0.0,0.0,   0.5,0.0,0.5,   0.5,0.5,0.0]);
+        drawTriangle3D( [0.5,0.0,0.5,   1.0,0.0,0.0,   0.5,0.5,0.0]);
+    
+        gl.uniform4f(u_FragColor, rgba[0]*0.5, rgba[1]*0.5, rgba[2]*0.5, rgba[3]);
+        drawTriangle3D( [0.0,0.0,0.0,   0.5,0.0,0.5,   1.0,0.0,0.0]);
+    }
+}
