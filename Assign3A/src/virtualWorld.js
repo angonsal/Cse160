5d8551678dc5a6ebdd3202bcfe6a27a5fa0dbcd7
Ashley Gonsalves <ashleygonsalves@eduroam-169-233-203-224.ucsc.edu>
2024-05-02 17:15:42 -0700

    3.1-3.2

--- Assign3A/src/virtualWorld.js
+++ Assign3A/src/virtualWorld.js
@@ -20,9 +20,11 @@ var FSHADER_SOURCE = `
   precision mediump float;
   varying vec2 v_UV; 
   uniform vec4 u_FragColor;
+  uniform sampler2D u_Sampler0; 
   void main() {
     gl_FragColor = u_FragColor;
     gl_FragColor = vec4(v_UV, 1,1);
+    gl_FragColor = texture2D(u_Sampler0, v_UV); 
   }`
 
 // Global 
@@ -34,6 +36,7 @@ let u_size;
 let a_UV; 
 let u_ModelMatrix;
 let u_GlobalRotateMatrix; 
+let u_Sampler0; 
 
 function setupWebGL() {
     // Retrieve <canvas> element
@@ -46,15 +49,62 @@ function setupWebGL() {
     return;
     }
   gl.enable(gl.DEPTH_TEST); 
+}
+
+function initTextures(gl, n) {
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
   }
+
+  // Get the storage location of u_Sampler
+  var u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+  if (!u_Sampler0) {
+    console.log('Failed to get the storage location of u_Sampler0');
+    return false;
+  }
+  var image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function(){ sendTexture(gl, n, texture, u_Sampler0, image); };
+  // Tell the browser to load an image
+  image.src = '../resources/sky.jpeg';
+
+  return true;
+}
+
+function sendTexture(gl, n, texture, u_Sampler0, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler0, 0);
+  
+  // gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+  console.log('finished loadTexture'); 
+  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+}
+
 
 
 function connectVariablesToGLSL(){
   // Initialize shaders
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
+    console.log('Failed to initialize shaders.');
     return;
-  }
+}
 
   // Get the storage location of a_Position
   a_Position = gl.getAttribLocation(gl.program, 'a_Position');
@@ -102,6 +152,12 @@ function connectVariablesToGLSL(){
     return;
   }
 
+  u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+    if (!u_Sampler0) {
+        console.log('Failed to get the storage location of u_Sampler0');
+        return false;
+    }
+
   var identityM = new Matrix4(); 
   gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
 
@@ -112,6 +168,7 @@ function connectVariablesToGLSL(){
   gl.uniformMatrix4fv(u_ViewMatrix, false, projectionMatrix.elements);
 
 }
+
 //constants 
 const POINT = 0;
 const TRIANGLE = 1; 
@@ -151,14 +208,16 @@ function main() {
   setupWebGL();
   connectVariablesToGLSL(); 
   addActionsHTMLUI();
+  initTextures(gl,0); 
   
   // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.85, 0.75, 1.0, 1.0);
 
   // Clear <canvas>
   // gl.clear(gl.COLOR_BUFFER_BIT);
   renderScene();
   requestAnimationFrame(tick);
+
 
 }
 
